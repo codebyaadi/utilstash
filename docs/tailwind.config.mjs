69e1f26bce5bdc09bc591894a40fb99df538b4e2
codebyaadi <adityastar57@gmail.com>
2024-09-07 01:25:03 +0530

    init: hero section base done

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(35, 100%, 65%)'
+      }
+    }
+  },
+  plugins: []
 }
