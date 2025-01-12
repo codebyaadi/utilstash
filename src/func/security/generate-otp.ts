ad3ba24291bfdc974d2558a228602b4f71151f1a
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2025-01-12 11:37:59 +0530

    bug: modify the code to throw proper error for invalid length while generating OTP

--- src/func/security/generate-otp.ts
+++ src/func/security/generate-otp.ts
@@ -3,8 +3,13 @@
  *
  * @param {4 | 6 | 8} [length=4] - The length of the OTP. Can be 4, 6, or 8. Defaults to 4.
  * @returns {string} The generated OTP as a string of digits.
+ * @throws {Error} If length is not 4, 6, or 8
  */
 export const generateOTP = (length: 4 | 6 | 8 = 4): string => {
+  if (![4, 6, 8].includes(length)) {
+    throw new Error('OTP length must be 4, 6, or 8 digits')
+  }
+
   const digits = '0123456789'
   let otp = ''
 
